56c469aec8ef39de0367ae0e3ec4e65c2110dfbc
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-25 23:25:58 +0600

    done

---
+++ Random/one.js
@@ -0,0 +1,10 @@
+const nana = "Emran";
+const jurha = " + "
+const nani = "Bushu";
+const love = nana + jurha + nani;
+const value = 100;
+if (value == 100) {
+console.log(love);
+} else {
+    console.log("Nani Rag Korcoin..");
+}
