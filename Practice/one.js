b7e5ad2a1d5ab6d743f3ea27fba8731ebc6c4097
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-27 14:42:08 +0600

    Update: New File Included

---
+++ Practice/one.js
@@ -0,0 +1,6 @@
+const name = "Parvej Husen Talukder";
+const age = 19;
+const category = 'Poet';
+console.log(name);
+console.log(age);
+console.log(category);
---
+++ Practice/two.js
@@ -0,0 +1,6 @@
+/*My JS Directory*/
+// Solving a Problem of Sum Two No.
+var thisYear = 2025;
+var fromYear = 2005;
+var sum = thisYear + fromYear;
+console.log("Sum of two numbers is:", sum);
