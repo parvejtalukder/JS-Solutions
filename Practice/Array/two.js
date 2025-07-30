0758697635ef4171101c198596749baeb7e817db
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-31 01:02:21 +0600

    Update: Another File

---
+++ Practice/Array/two.js
@@ -0,0 +1,15 @@
+/* Add or remove elements
+Declare an array of 3 tourist destinations
+Add a new tourist destination to your tourist array
+Add two more to your array
+Remove the last tourist destination you have added
+display the final array as output*/
+
+let place = ["Niladri", "Jaflong", "Tekerghat"];
+console.log("The Initial Array: "+ place + "\n");
+place.push("Srimangal");
+console.log("A Place added to Array: "+ place + "\n");
+place.push("SunarGaon", "Cox's Bazar");
+console.log("More Two Place Added to Array: "+ place + "\n");
+place.pop();
+console.log("The Final Array: "+ place);
