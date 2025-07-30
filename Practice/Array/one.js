a1274a72f05541d44b5b94e12dd72ed7933671a5
parvejtalukder <mail@parvejhusentalukder.com>
2025-07-31 00:57:24 +0600

    Update: Practicing Array

---
+++ Practice/Array/one.js
@@ -0,0 +1,12 @@
+/*Declare an array
+Declare an array with 5 elements containing fruits
+console log the 3rd index element
+change the value of the 2nd index element to jambura
+console log the final array*/
+
+let nums = [1, 2, 3, 4, 5];
+console.log("3rd indx is "+ nums[2]);
+console.log("2nd indx is "+ nums[1]);
+nums[1] = 50;
+console.log("Changed 2nd indx is "+ nums[1]);
+console.log("\nThe Final Array Below:\n"+ nums);
--- Practice/two.js
+++ Practice/two.js
@@ -1,6 +1,18 @@
-/*My JS Directory*/
-// Solving a Problem of Sum Two No.
-var thisYear = 2025;
-var fromYear = 2005;
-var sum = thisYear + fromYear;
-console.log("Sum of two numbers is:", sum);
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("Enter a number: ", function(input) {
+  const n = parseInt(input);
+
+  if (n % 2 === 0) {
+    console.log("Even");
+  } else {
+    console.log("Odd");
+  }
+
+  rl.close();
+});
