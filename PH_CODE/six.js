b7cac458628a9b1af65b788ed332386b3634a9da
parvejtalukder <mail@parvejhusentalukder.com>
2025-08-04 19:29:58 +0600

    Update: Files

--- PH_CODE/six.js
+++ PH_CODE/six.js
@@ -1,6 +1,10 @@
 /** Problem 06 :  (Current Salary )  */
 var experience = 30;
 var startingSalary = 45000;
-var intcreYear = 5/100;
 
- 
+var incre_per_year = 5 / 100;  
+var salary = startingSalary;
+for (var year = 1; year <= experience; year++) {
+    salary = salary + (salary * incre_per_year);
+}
+console.log(salary.toFixed(2));
